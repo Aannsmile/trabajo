5ecee771557faf9910120d0d988cdbbe9a799836
AzureAD\AndreaCosioFernández <2021345@iesluisbraille.es>
2022-01-27 09:18:53 +0100

    menú gráfico

---
+++ ppt.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 09:02:03 2022
+
+@author: AndreaCosioFernández
+"""
+
+print("Bienvenido al piedra, papel, tijera online")
+print("---------------------------------------------")
+print("|                 ♥ MENÚ ♥                  |")                
+print("---------------------------------------------")
+print("| 1.Elegir nicks de jugadores               |")
+print("| 2.Partida rapida                          |")
+print("| 3.Partida completa                        |")
+print("| 4.Salir                                   |")
+print("| 5.Creditos                                |")
+print("---------------------------------------------")
