2f89c9dcc7ac016af39769f11523ba5826b7b7d1
AzureAD\AndreaCosioFernández <2021345@iesluisbraille.es>
2022-01-27 13:12:35 +0100

    menu1

--- ppt.py
+++ ppt.py
@@ -9,9 +9,17 @@ print("Bienvenido al piedra, papel, tijera online")
 print("---------------------------------------------")
 print("|                 ♥ MENÚ ♥                  |")                
 print("---------------------------------------------")
-print("| 1.Elegir nicks de jugadores               |")
+print("| 1.Elegir nicks del jugador                |")
 print("| 2.Partida rapida                          |")
 print("| 3.Partida completa                        |")
 print("| 4.Salir                                   |")
 print("| 5.Creditos                                |")
 print("---------------------------------------------")
+
+valormenu=int(input("Seleccione una opción:"))
+
+if valormenu==1:
+    nick=int(input("Seleccione nick:"))
+    
+
+    
