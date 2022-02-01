fa161a1d30966abbaf75b7524412d0002ce88bff
AzureAD\AndreaCosioFernández <2021345@iesluisbraille.es>
2022-02-01 12:55:19 +0100

    p_rapida

--- ppt.py
+++ ppt.py
@@ -4,6 +4,7 @@ Created on Thu Jan 27 09:02:03 2022
 
 @author: AndreaCosioFernández
 """
+import random
 
 print("Bienvenido al piedra, papel, tijera online")
 print("---------------------------------------------")
@@ -16,10 +17,46 @@ print("| 4.Salir                                   |")
 print("| 5.Creditos                                |")
 print("---------------------------------------------")
 
+piedra=1
+papel=2
+tijera=3
+
 valormenu=int(input("Seleccione una opción:"))
 
 if valormenu==1:
     nick=int(input("Seleccione nick:"))
+    
+elif valormenu==2:
+    eleccion=int(input("Teniendo en cuenta que el 1=piedra, el 2=papel y el 3=tijera, seleccione un número del 1 al 3:"))
+    if eleccion <3 or eleccion >1:
+        print(f"Has elegido {eleccion}")
+        maquina=random.randrange(3)
+        print(f"{nick} ha sacado {eleccion} y la maquina ha sacado{maquina}")
+        if eleccion==1:
+            if maquina==1:
+                print(f"La máquina y tu {nick} habeis empatado") 
+            elif maquina==2:
+                print("Ha ganado la Maquina :(")
+            elif maquina==3:
+                print("Has ganado tú :)")
+                    
+        if eleccion==2:
+            if maquina==1:
+                print("Has ganado tú :)") 
+            elif maquina==2:
+                print(f"La máquina y tu {nick} habeis empatado")
+            elif maquina==3:
+                print("Ha ganado la Maquina :(")
+                    
+        elif eleccion==3:
+            if maquina==1:
+                print("Ha ganado la Maquina :(") 
+            elif maquina==2:
+                print("Has ganado tú :)")
+            elif maquina==3:
+                print(f"La máquina y tu {nick} habeis empatado")
+
+    
     
 
     
