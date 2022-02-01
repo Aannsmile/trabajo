924a2b3406f8e0da0f482ffdea45f19a64e1b8c9
AzureAD\AndreaCosioFernández <2021345@iesluisbraille.es>
2022-02-01 13:12:45 +0100

    spock

--- ppt.py
+++ ppt.py
@@ -20,6 +20,8 @@ print("---------------------------------------------")
 piedra=1
 papel=2
 tijera=3
+lagarto=4
+spock=5
 
 valormenu=int(input("Seleccione una opción:"))
 
@@ -28,7 +30,7 @@ if valormenu==1:
     
 elif valormenu==2:
     eleccion=int(input("Teniendo en cuenta que el 1=piedra, el 2=papel y el 3=tijera, seleccione un número del 1 al 3:"))
-    if eleccion <3 or eleccion >1:
+    if eleccion <=3 or eleccion >=1:
         print(f"Has elegido {eleccion}")
         maquina=random.randrange(3)
         print(f"{nick} ha sacado {eleccion} y la maquina ha sacado{maquina}")
@@ -56,6 +58,72 @@ elif valormenu==2:
             elif maquina==3:
                 print(f"La máquina y tu {nick} habeis empatado")
 
+elif valormenu==3:
+    eleccionp=int(input("Teniendo en cuenta que el 1=piedra, el 2=papel, el 3=tijera, el 4=lagarto y el 5=spock seleccione un número del 1 al 5:"))
+    if eleccionp <=5 or eleccion >=1:
+        print(f"Has elegido {eleccionp}")
+        maquinap=random.randrange(5)
+        print(f"{nick} ha sacado {eleccion} y la maquina ha sacado{maquina}")
+
+    if eleccion==1:#piedra
+        if maquina==1:
+            print(f"La máquina y tu {nick} habeis empatado") 
+        elif maquina==2:
+            print("Ha ganado la Maquina :(")
+        elif maquina==3:
+            print("Has ganado tú :)")
+        elif maquina==4:
+            print("Has ganado tú :)")
+        elif maquina==5:
+            print("Ha ganado la Maquina :(")
+                
+    if eleccion==2:#papel
+        if maquina==1:
+            print("Has ganado tú :)") 
+        elif maquina==2:
+            print(f"La máquina y tu {nick} habeis empatado")
+        elif maquina==3:
+            print("Ha ganado la Maquina :(")
+        elif maquina==4:
+            print("Ha ganado la Maquina :(")
+        elif maquina==5:
+            print("Has ganado tú :)")
+                
+    elif eleccion==3:#tijera
+        if maquina==1:
+            print("Ha ganado la Maquina :(") 
+        elif maquina==2:
+            print("Has ganado tú :)")
+        elif maquina==3:
+             print(f"La máquina y tu {nick} habeis empatado")   
+        elif maquina==4:
+            print("Has ganado tú :)")
+        elif maquina==5:
+            print("Ha ganado la Maquina :(")
+    
+    elif eleccion==4:#lagarto
+        if maquina==1:
+            print("Ha ganado la Maquina :(") 
+        elif maquina==2:
+            print("Has ganado tú :)")
+        elif maquina==3:
+            print("Ha ganado la Maquina :(")    
+        elif maquina==4:
+            print(f"La máquina y tu {nick} habeis empatado")
+        elif maquina==5:
+            print("Has ganado tú :)")
+            
+    elif eleccion==5:#spock
+        if maquina==1:
+            print("Has ganado tú :)")
+        elif maquina==2:
+            print("Ha ganado la Maquina :(")
+        elif maquina==3:
+            print("Has ganado tú :)")
+        elif maquina==4:
+            print("Ha ganado la Maquina :(")
+        elif maquina==5:
+            print(f"La máquina y tu {nick} habeis empatado")
     
     
 
